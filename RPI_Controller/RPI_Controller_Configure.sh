103f03e184d3026463037ee9c98f9321530e0853
nathgibb <nathgibb@ip-172-31-59-76.ec2.internal>
2016-11-17 16:07:12 -0500

    adding new files for cron

--- RPI_Controller/RPI_Controller_Configure.sh
+++ RPI_Controller/RPI_Controller_Configure.sh
@@ -30,17 +30,35 @@ done
 #
 # Checking for configuration files
 #
-if [ -f "$rpi_Controller_Config" ];
-then
+if [ -f "$rpi_Controller_Config" ];then
   #dialog --infobox "Reading existing configuration file" 10 30 ; sleep 20
-  for i in $(seq 0 10 100) ; do sleep 1; clear; echo $i | dialog --gauge "Reading existing configuration file" 10 70 0; done
+  #for i in $(seq 0 10 100) ; do sleep 1; clear; echo $i | dialog --gauge "Reading existing configuration file" 10 70 0; done
+  echo "not here"
    #echo "File $rpi_Controller_Config exists."
 else
   #dialog --infobox "Reading existing configuration file" 10 30 ; sleep 20
    #echo "File $rpi_Controller_Config does not exist."
-   for i in $(seq 0 10 100) ; do sleep 1; clear; echo $i | dialog --gauge "No configuration file found" 10 70 0; done
-fi
+   dialog --infobox "No configuration file found.\nCreating default configuration file" 5 40 ; sleep 2
+   #for i in $(seq 0 10 100) ; do sleep 1; clear; echo $i | dialog --gauge "No configuration file found" 10 70 0; done
+   echo 10 | dialog --title "Installing scripts" --gauge "Creating directory /ST" 10 70 0;sleep 1;clear
+   if [ ! -d "/ST" ];then
+     mkdir "/ST"
+   fi
+   echo 20 | dialog --title "Installing scripts" --gauge "Cloning ST github repo" 10 70 0;sleep 1;clear
+   if [ ! -d "/ST/.git" ];then
+      git clone https://github.com/NAGibb/ST /ST
+   else
+     git clone https://github.com/NAGibb/ST /ST
+   fi
 
+
+   echo 30 | dialog --title "Installing scripts" --gauge "Reading existing configuration file" 10 70 0;sleep 1;clear
+
+   #  create /ST
+   #  create /ST/RPI_Controller
+   #  create default rpi_Controller_Config.cfg config file
+
+fi
 
 
 
--- RPI_Controller/cronScheduler.sh
+++ RPI_Controller/cronScheduler.sh
@@ -1,1 +1,13 @@
 #!/bin/sh
+
+
+quarterpidfile=/var/run/RPI_Controller_quarter.pid
+halfpidfile=/var/run/RPI_Controller_half.pid
+fullpidfile=/var/run/RPI_Controller_full.pid
+daypidfile=/var/run/RPI_Controller_day.pid
+
+# Ensure PID file is removed on program exit.
+trap "rm -f -- '$mypidfile'" EXIT
+
+# Create a file with current PID to indicate that process is running.
+echo $$ > "$mypidfile"
