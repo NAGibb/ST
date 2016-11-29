00abeef943e65345449c0f52c8f22d98d1b9698c
nathgibb <nathgibb@ip-172-31-59-76.ec2.internal>
2016-11-29 15:41:06 -0500

    daily changes

--- RPI_Controller/cronScheduler.sh
+++ RPI_Controller/cronScheduler.sh
@@ -8,6 +8,86 @@ crondevice="unknown"
 croncommand="unknown"
 cronstatusreturn="unknown"
 cronoutput="unknown"
+cronopts=0
+logfile="/var/log/cronscheduler.log"
+#
+#
+# Functions
+#
+#
+logger ()
+{
+# function options
+# $1 level low to high {1-4}
+# $2 msg
+#
+#
+# set variablesloglevel=0
+
+loglevel=""
+logmessage=""
+
+  # Check the log level
+    case ${1} in
+      1)
+        loglevel="low"
+      ;;
+      2)
+        loglevel="medium"
+      ;;
+      3)
+        loglevel="high"
+      ;;
+      4)
+        loglevel="security"
+      ;;
+      *)
+        # first option is not a number. Set log level to low and use the input for the message so we dont lose anything important
+        loglevel="low"
+        logmessage="misconfigured log level - ${1} / ${2}"
+      ;;
+    esac
+
+  # Check the message for special instructions
+    case ${2} in
+      -blank)
+        logmessage=" "
+        loglevel="supress"
+      ;;
+      -section)
+        logmessage="_______________________________________________________________________________________________"
+        loglevel="supress"
+      ;;
+      -header)
+        logmessage="==============================================================================================="
+        loglevel="supress"
+      ;;
+      *)
+        logmessage=${2}
+      ;;
+    esac
+
+    if [ $loglevel = "supress" ]; then
+        # separators not level
+        #echo `date +"%D %H:%M"` " ${logmessage}"
+        echo `date +"%D %H:%M"` " ${logmessage}" >> $logfile
+    else
+        # normal output
+        #echo `date +"%D %H:%M"` " - ${loglevel} - ${logmessage}"
+        echo `date +"%D %H:%M"` " - ${loglevel} - ${logmessage}" >> $logfile
+    fi
+
+}
+
+# check root status stop here if not root
+if [ "$(id -u)" != "0" ]; then
+   echo "Configuration script must be run as root. Try again." 1>&2
+   exit 1
+   logger 0 "-header"
+   logger 4 "Configuration script must be run as root. Try again."
+   logger 0 "-header"
+
+fi
 #
 # get script options and set variables
 while [ $# -gt 1 ]
@@ -18,15 +98,23 @@ do
       case $2 in
         quarter)
             croninterval="quarter"
+            # increment cronopts for a good option
+            cronopts=$((cronopts+1))
         ;;
         half)
             croninterval="half"
+            # increment cronopts for a good option
+            cronopts=$((cronopts+1))
         ;;
         full)
             croninterval="full"
+            # increment cronopts for a good option
+            cronopts=$((cronopts+1))
         ;;
         day)
             croninterval="day"
+            # increment cronopts for a good option
+            cronopts=$((cronopts+1))
         ;;
         *)
             croninterval="unknown"
@@ -38,12 +126,23 @@ do
       case $2 in
         add)
           cronaction="add"
+          # increment cronopts for a good option
+          cronopts=$((cronopts+1))
+        ;;
+        run)
+          cronaction="run"
+          # increment cronopts for a good option
+          cronopts=$((cronopts+1))
         ;;
         delete)
           cronaction="delete"
+          # increment cronopts for a good option
+          cronopts=$((cronopts+1))
         ;;
         print)
           cronaction="print"
+          # increment cronopts for a good option
+          cronopts=$((cronopts+1))
         ;;
         *)
           cronaction="unknown"
@@ -53,19 +152,37 @@ do
     shift
     ;;
     -d|--device)
-      crondevice="$2"
+      case $2 in
+        "")
+          crondevice="unknown"
+        ;;
+        -*)
+          crondevice="unknown"
+        ;;
+        *)
+        crondevice="${2}"
+        # increment cronopts for a good option - here its any option
+        cronopts=$((cronopts+1))
+        ;;
+      esac
       shift
     ;;
     -c|--command)
       case $2 in
         ping)
           croncommand="ping"
+          # increment cronopts for a good option
+          cronopts=$((cronopts+1))
         ;;
         http)
           croncommand="http"
+          # increment cronopts for a good option
+          cronopts=$((cronopts+1))
         ;;
         *)
           croncommand="unknown"
+          # increment cronopts for a good option
+          cronopts=$((cronopts+1))
         ;;
       esac
     shift
@@ -74,12 +191,18 @@ do
       case $2 in
         msg)
           cronstatusreturn="msg"
+          # increment cronopts for a good option
+          cronopts=$((cronopts+1))
         ;;
         onoff)
           cronstatusreturn="onoff"
+          # increment cronopts for a good option
+          cronopts=$((cronopts+1))
         ;;
         status)
           cronstatusreturn="status"
+          # increment cronopts for a good option
+          cronopts=$((cronopts+1))
         ;;
         *)
           croncommand="unknown"
@@ -88,25 +211,63 @@ do
     shift
     ;;
     -o|--output)
-      cronoutput="${2}"
+      case $2 in
+        "")
+          cronoutput="unknown"
+        ;;
+        -*)
+          cronoutput="unknown"
+        ;;
+        *)
+        cronoutput="${2}"
+        # increment cronopts for a good option - here its any option
+        cronopts=$((cronopts+1))
+        ;;
+      esac
       shift
     ;;
-
-# functions needed
-# ping / up
   esac
   shift # past argument or value
 done
 
+  logger 0 "-header"
+  logger 1 "Starting script"
+  logger 0 "-header"
+  logger 0 "-blank"
+# check to see if we have all the information needed to execute
+
+# check to see if this is just a simple run job
+if [ ${croninterval} != "unknown" ] && [ ${cronaction} = "run" ]; then
+  logger 1 "doing cron run"
+  touch ${logfile}
+# check to see if this is  complex action
+else
+  if [ ${cronopts} = 6 ];then
+    echo "doing complex job"
+  else
+    echo "Incorrect number of options. Here are the options that have been detected."
+    echo "Make sure that none of these options are showing unknown"
+    echo "Number of good options: " ${cronopts}
+    echo interval  = "${croninterval}"
+    echo action     = "${cronaction}"
+    echo device     = "${crondevice}"
+    echo command    = "${croncommand}"
+    echo statusreturn = "${cronstatusreturn}"
+    echo output     =   "${cronoutput}"
+  fi
+fi
 
 
 
-echo interval  = "${croninterval}"
-echo action     = "${cronaction}"
-echo device     = "${crondevice}"
-echo command    = "${croncommand}"
-echo statusreturn = "${cronstatusreturn}"
-echo output     =   "${cronoutput}"
+
+
+
+# functions needed
+# ping / up
+# logger
+# when checking options for run only need interval and run options for all
+# others its the full list
+
 
 
 
@@ -139,3 +300,7 @@ echo output     =   "${cronoutput}"
 
 # Create a file with current PID to indicate that process is running.
 #echo $$ > "$mypidfile"
+logger 0 "-blank"
+logger 0 "-header"
+logger 1 "Stopping script"
+logger 0 "-header"
